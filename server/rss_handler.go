63503c1c0f5f41b3a44e09c607996e07cb99237d
Christopher Brown <ccbrown112@gmail.com>
2017-02-11 01:31:17 -0800

    fix: rss 2.0 conformance and recommendations

--- server/rss_handler.go
+++ server/rss_handler.go
@@ -7,71 +7,99 @@ import (
 	"github.com/labstack/echo"
 )
 
+type rssGUID struct {
+	IsPermalink bool   `xml:"isPermaLink,attr"`
+	GUID        string `xml:",chardata"`
+}
+
 type rssItem struct {
-	Title       string `xml:"title"`
-	GUID        string `xml:"guid"`
-	Description string `xml:"description"`
-	Link        string `xml:"link"`
-	PubDate     string `xml:"pubDate"`
+	Title       string  `xml:"title"`
+	GUID        rssGUID `xml:"guid"`
+	Description string  `xml:"description"`
+	Link        string  `xml:"link"`
+	PubDate     string  `xml:"pubDate"`
+}
+
+type rssAtomLink struct {
+	HRef string `xml:"href,attr"`
+	Rel  string `xml:"rel,attr"`
+	Type string `xml:"type,attr"`
 }
 
 type rssChannel struct {
-	Title       string `xml:"title"`
-	Description string `xml:"description"`
-	Link        string `xml:"link"`
+	Title       string      `xml:"title"`
+	Description string      `xml:"description"`
+	Link        string      `xml:"link"`
+	AtomLink    rssAtomLink `xml:"atom:link"`
+	Items       []rssItem   `xml:"item"`
 }
 
 type rssResponse struct {
 	XMLName bool       `xml:"rss"`
-	Version int        `xml:"version,attr"`
+	Version string     `xml:"version,attr"`
+	Atom    string     `xml:"xmlns:atom,attr"`
 	Channel rssChannel `xml:"channel"`
-	Items   []rssItem  `xml:"item"`
 }
 
 func RSSHandler(db *Database) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		activity, _ := db.Activity(c.QueryParam("next"), 50)
 		response := rssResponse{
-			Version: 2,
+			Version: "2.0",
+			Atom:    "http://www.w3.org/2005/Atom",
 			Channel: rssChannel{
 				Title:       "GGG Tracker",
 				Description: "Latest activity from Grinding Gear Games",
 				Link:        AbsoluteURL(c, ""),
+				AtomLink: rssAtomLink{
+					HRef: AbsoluteURL(c, "/rss"),
+					Rel:  "self",
+					Type: "application/rss+xml",
+				},
 			},
 		}
 		for _, a := range activity {
 			switch a.(type) {
 			case *ForumPost:
 				post := a.(*ForumPost)
-				response.Items = append(response.Items, rssItem{
-					Title:       post.Poster + " - " + post.ThreadTitle,
-					Link:        post.PostURL(),
-					GUID:        fmt.Sprintf("poe-forum-post-%v", post.Id),
+				response.Channel.Items = append(response.Channel.Items, rssItem{
+					Title: post.Poster + " - " + post.ThreadTitle,
+					Link:  post.PostURL(),
+					GUID: rssGUID{
+						IsPermalink: false,
+						GUID:        fmt.Sprintf("poe-forum-post-%v", post.Id),
+					},
 					Description: post.BodyHTML,
 					PubDate:     post.Time.Format(time.RFC1123Z),
 				})
 			case *RedditComment:
 				comment := a.(*RedditComment)
-				response.Items = append(response.Items, rssItem{
-					Title:       comment.Author + " - " + comment.PostTitle,
-					Link:        comment.CommentURL(),
-					GUID:        "reddit-comment-" + comment.Id,
+				response.Channel.Items = append(response.Channel.Items, rssItem{
+					Title: comment.Author + " - " + comment.PostTitle,
+					Link:  comment.CommentURL(),
+					GUID: rssGUID{
+						IsPermalink: false,
+						GUID:        "reddit-comment-" + comment.Id,
+					},
 					Description: comment.BodyHTML,
 					PubDate:     comment.Time.Format(time.RFC1123Z),
 				})
 			case *RedditPost:
 				post := a.(*RedditPost)
 				item := rssItem{
-					Title:       post.Author + " - " + post.Title,
-					Link:        "https://www.reddit.com" + post.Permalink,
-					GUID:        "reddit-post-" + post.Id,
+					Title: post.Author + " - " + post.Title,
+					Link:  "https://www.reddit.com" + post.Permalink,
+					GUID: rssGUID{
+						IsPermalink: false,
+						GUID:        "reddit-post-" + post.Id,
+					},
 					Description: post.BodyHTML,
 					PubDate:     post.Time.Format(time.RFC1123Z),
 				}
 				if item.Description == "" {
 					item.Description = "<a href=\"" + post.URL + "\">" + post.URL + "</a>"
 				}
-				response.Items = append(response.Items, item)
+				response.Channel.Items = append(response.Channel.Items, item)
 			}
 		}
 		return c.XML(200, response)
